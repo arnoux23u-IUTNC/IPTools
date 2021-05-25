dafbaca32ae21c8328c05572e12607a4dd6873c0
guillaumearnx <37373941+guillaumearnx@users.noreply.github.com>
2021-05-25 21:58:27 +0200

    Final script

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,27 +4,24 @@ let isIP = (ipaddress) => {
     return /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(ipaddress);
 }
 
-let ipvalue, iplength, sendButton, binaryIP;
+let ipvalue, iplength, sendButton;
 
 //values
 let pIP, pMask, pNetwork, pBroadcast, pFirst, pLast, pNb
 
-let decompose = (ip, length) => {
-    if (isNaN(length) || typeof length === undefined || length === null) {
-        length = 24;
+let parse = (ip) => {
+    let s = ""
+    if (ip.length === 32) {
+        for (let i = 1; i <= 32; i++) {
+            s += ip.substring(i - 1, i)
+            if (i !== 1 && i !== 32 && i % 8 === 0) {
+                s += "."
+            }
+        }
+        return s;
     }
-
+    return null;
 }
-
-let ipToBinary = (input) => {
-    const fields = input.split('.');
-    const f1 = ("00000000" + parseInt(fields[0]).toString(2)).slice(-8);
-    const f2 = ("00000000" + parseInt(fields[1]).toString(2)).slice(-8);
-    const f3 = ("00000000" + parseInt(fields[2]).toString(2)).slice(-8);
-    const f4 = ("00000000" + parseInt(fields[3]).toString(2)).slice(-8);
-    return f1 + "." + f2 + "." + f3 + "." + f4;
-}
-
 let binaryToIP = (input) => {
     const fields = input.split('.');
     const f1 = parseInt(fields[0], 2)
@@ -34,67 +31,57 @@ let binaryToIP = (input) => {
     return f1 + "." + f2 + "." + f3 + "." + f4;
 }
 
+let ipToBinary = (input) => {
+    const fields = input.split('.');
+    const f1 = ("00000000" + parseInt(fields[0]).toString(2)).slice(-8);
+    const f2 = ("00000000" + parseInt(fields[1]).toString(2)).slice(-8);
+    const f3 = ("00000000" + parseInt(fields[2]).toString(2)).slice(-8);
+    const f4 = ("00000000" + parseInt(fields[3]).toString(2)).slice(-8);
+    return f1 + f2 + f3 + f4;
+}
+
 let getMasque = (length) => {
     let s = "";
     for (let i = 0; i < 32; i++) {
-        if (i != 0 && i != "0" && i != 32 && i != "32" && i % 8 == 0) {
-            s += "."
-        }
         if (i >= length) {
             s += "0"
         } else {
             s += "1"
         }
     }
-    return binaryToIP(s)
+    return binaryToIP(parse(s))
 }
 
 let getNetwork = (ipBIN, length) => {
-    let iv = (Math.floor(length / 8));
-    let s = ipBIN.substring(0, parseInt(length) + parseInt(iv));
-    console.log(s);
-    for (let i = parseInt(length) + parseInt(iv) + 1; i < 32; i++) {
-        console.log(" i : " + i);
-        if (i != 0 && i != "0" && i != 32 && i != "32" && i % 8 == 0) {
-            console.log("add . at " + i);
-            s += "."
-        }
-        console.log("add 0 at " + i);
-        s += "0"
-    }
-    console.log(s);
-    return binaryToIP(s)
+    length = parseInt(length)
+    let s = ipBIN.substring(0, length);
+    let s2 = ipBIN.substring(length);
+    s += s2.replace(/1/g, '0');
+    return binaryToIP(parse(s))
 }
 
 let getBroadcast = (ip, length) => {
-    let s = ip.substring(0, length + 2);
-    for (let i = 32; i >= length; i--) {
-        s += "1"
-    }
-    console.log("resobin : " + s);
-    return binaryToIP(s)
+    length = parseInt(length)
+    let s = ip.substring(0, length);
+    let s2 = ip.substring(length);
+    s += s2.replace(/0/g, '1');
+    return binaryToIP(parse(s))
 }
 
 let getF = (ip, length) => {
-    let s = ip.substring(0, length);
-    for (let i = 32; i > length; i--) {
-        s += "0"
-    }
-    s += "1"
-    console.log("first : " + s);
-    return binaryToIP(s)
+    let ipT = ipToBinary(getNetwork(ip, length))
+    let ipT2 = ipT.substring(ipT.length - 1)
+    ipT = ipT.substring(0, ipT.length - 1) + ipT2.replace(/0/g, '1')
+    return binaryToIP(parse(ipT))
 }
 let getL = (ip, length) => {
-    let s = ip.substring(0, length);
-    for (let i = 32; i > length; i--) {
-        s += "1"
-    }
-    s += 0
-    console.log("last : " + s);
-    return binaryToIP(s)
+    let ipT = ipToBinary(getBroadcast(ip, length))
+    let ipT2 = ipT.substring(ipT.length - 1)
+    ipT = ipT.substring(0, ipT.length - 1) + ipT2.replace(/1/g, '0')
+    return binaryToIP(parse(ipT))
 }
 let getNb = (length) => {
-    return Math.pow((32 - length), 2) - 2;
+    return Math.pow(2, (32 - length)) - 2;
 }
 
 
@@ -113,14 +100,15 @@ window.onload = () => {
 
     sendButton.addEventListener("click", () => {
         if (isIP(ipvalue.value)) {
-            let binaryIP = ipToBinary(ipvalue.value)
-            pIP.innerHTML = `Adresse : ${ipvalue.value}/${iplength.value}`;
-            pMask.innerHTML = `Masque : ${getMasque(iplength.value)}`;
-            pNetwork.innerHTML = `Network : ${getNetwork(binaryIP, iplength.value)}`;
-            /*pBroadcast.innerHTML = `Broadcast : ${getBroadcast(ip.value, length)}`;
-            pFirst.innerHTML = `First : ${getF(ip.value, length)}`;
-            pLast.innerHTML = `Last : ${getL(ip.value, length)}`;
-            pNb.innerHTML = `Nb de machines : ${getNb(length)}`;*/
+            const lengthIP = (iplength.value > 0 && iplength.value > 33) ? iplength.value : 24
+            let binaryIP2 = ipToBinary(ipvalue.value)
+            pIP.innerHTML = `Adresse : ${ipvalue.value}/${lengthIP}`;
+            pMask.innerHTML = `Masque : ${getMasque(lengthIP)}`;
+            pNetwork.innerHTML = `Network : ${getNetwork(binaryIP2, lengthIP)}`;
+            pBroadcast.innerHTML = `Broadcast : ${getBroadcast(binaryIP2, lengthIP)}`;
+            pFirst.innerHTML = `First : ${getF(binaryIP2, lengthIP)}`;
+            pLast.innerHTML = `Last : ${getL(binaryIP2, lengthIP)}`;
+            pNb.innerHTML = `Nb de machines : ${getNb(lengthIP)}`;
         }
     })
 }
